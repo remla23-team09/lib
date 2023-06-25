509eaa1917c00748193c7c2575392d52e2f90005
Zhiyong <200~z13913982280@gmail.com>
2023-06-26 00:26:15 +0200

    change name

--- src/VersionUtil.py
+++ src/VersionUtil.py
@@ -5,7 +5,16 @@ class VersionUtil:
     @staticmethod
     def get_version():
         try:
-            version = subprocess.check_output(["git", "describe", "--tags"]).decode().strip()
+            version_info = subprocess.check_output(["git", "describe", "--tags"]).decode().strip()
+
+            version_parts = version_info.split('-')
+
+            if len(version_parts) > 1:
+                tag, commits, commit_hash = version_parts[:3]
+                version = tag[1:]
+            else:
+                version = version_info
+
         except subprocess.CalledProcessError:
             version = "Unknown"
 
