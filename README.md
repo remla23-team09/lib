3cd66a3a1d296431a2ca1eacb1ebb6ac0377ad63
Zhiyong <200~z13913982280@gmail.com>
2023-06-26 00:28:58 +0200

    change name

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # lib
-It is a version-aware library, run 'pip install REMLA23-team09-lib==0.0.1' to install the lib. 
+It is a version-aware library, run 'pip install REMLA23Team09LibProject==0.0.1' to install the lib. 
 You can use "VersionUtil.get_version()" to check the version.
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from src.VersionUtil import VersionUtil
 
 
 setup(
-    name='REMLA23Team09Lib',
+    name='REMLA23Team09LibProject',
     version=VersionUtil.get_version(),
     description='A version-aware library',
     author='Zhiyong Zhu',
     author_email='z13913982280@gmail.com',
     url='https://github.com/remla23-team09/lib',
     license='MIT',
-    long_description="REMLA23Team09Lib",
+    long_description="REMLA23Team09LibProject",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests',
