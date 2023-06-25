f8f54d08f0703a5b50bdd291bfbe9689784ee3b1
Zhiyong <200~z13913982280@gmail.com>
2023-06-26 01:19:20 +0200

    change name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from src.VersionUtil import VersionUtil
 
 
 setup(
-    name='REMLA23Team09LibProject',
+    name='PLZREMLA23Team09LibProject',
     version=VersionUtil.get_version(),
     description='A version-aware library',
     author='Zhiyong Zhu',
     author_email='z13913982280@gmail.com',
     url='https://github.com/remla23-team09/lib',
     license='MIT',
-    long_description="REMLA23Team09LibProject",
+    long_description="PLZREMLA23Team09LibProject",
     packages=setuptools.find_packages(),
     install_requires=[
         'requests',
